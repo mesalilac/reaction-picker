fbb3c37db047a387055086ed55e0c0bebffd68c7
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-09 10:32:43 +0300

    remove unused util functions

--- src-tauri/src/utils/fs.rs
+++ src-tauri/src/utils/fs.rs
@@ -1,20 +1,6 @@
 use crate::APP_NAME;
 use std::path::PathBuf;
 
-#[allow(dead_code)]
-pub fn get_cache_dir() -> PathBuf {
-    let cache_dir = directories::BaseDirs::new()
-        .expect("Failed to get base dir")
-        .cache_dir()
-        .join(APP_NAME);
-
-    if !cache_dir.exists() {
-        std::fs::create_dir_all(&cache_dir).expect("Failed to create cache dir");
-    }
-
-    cache_dir
-}
-
 pub fn get_app_data_dir() -> PathBuf {
     let app_data_dir = directories::BaseDirs::new()
         .expect("Failed to get base dir")
@@ -26,18 +12,4 @@ pub fn get_app_data_dir() -> PathBuf {
     }
 
     app_data_dir
-}
-
-#[allow(dead_code)]
-pub fn get_config_dir() -> PathBuf {
-    let config_dir = directories::BaseDirs::new()
-        .expect("Failed to get base dir")
-        .config_dir()
-        .join(APP_NAME);
-
-    if !config_dir.exists() {
-        std::fs::create_dir_all(&config_dir).expect("Failed to create config dir");
-    }
-
-    config_dir
 }
