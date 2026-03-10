adecade03e613ecc51fe08a1535d7c1223b63bc9
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-10 16:07:36 +0300

    remove redundant closure

--- src-tauri/src/commands/get.rs
+++ src-tauri/src/commands/get.rs
@@ -112,10 +112,7 @@ pub async fn get_tags(state: AppState<'_>) -> CommandResult<Vec<Tag>> {
 
     let tag_entities = tags::table.load::<TagEntity>(&mut conn)?;
 
-    let data: Vec<Tag> = tag_entities
-        .into_iter()
-        .map(|x| Tag::from_entity(x))
-        .collect();
+    let data: Vec<Tag> = tag_entities.into_iter().map(Tag::from_entity).collect();
 
     Ok(data)
 }
