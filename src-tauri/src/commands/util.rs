243ddc3906c9d63cd2ac971d38f92168cbafb43a
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-10 23:00:50 +0300

    parse video metadata

--- src-tauri/src/commands/util.rs
+++ src-tauri/src/commands/util.rs
@@ -1,8 +1,14 @@
 use super::prelude::*;
-use crate::utils::fs::get_app_images_dir;
+use crate::utils::fs::{get_app_images_dir, get_app_videos_dir};
 use diesel::dsl::{exists, select};
 use image::{EncodableLayout, ImageReader};
 use std::{io::Cursor, path::PathBuf};
+use symphonia::core::{
+    formats::FormatOptions,
+    io::MediaSourceStream,
+    meta::{MetadataOptions, StandardTagKey},
+    probe::Hint,
+};
 use walkdir::WalkDir;
 
 #[tauri::command]
@@ -18,7 +24,7 @@ pub async fn util_drop_files(state: AppState<'_>, paths: Vec<PathBuf>) -> Comman
         {
             let entry_path = entry.path();
             let file_bytes = std::fs::read(entry_path)?;
-            let cursor = Cursor::new(file_bytes.as_slice());
+            let cursor = Cursor::new(file_bytes.clone());
             let Some(file_name) = entry_path
                 .file_name()
                 .map(|s| s.to_string_lossy().to_string())
@@ -96,6 +102,79 @@ pub async fn util_drop_files(state: AppState<'_>, paths: Vec<PathBuf>) -> Comman
 
                 if video_exists {
                     continue;
+                }
+
+                let mss = MediaSourceStream::new(Box::new(cursor), Default::default());
+                let hint = Hint::new();
+                let format_opts = FormatOptions::default();
+                let metadata_opts = MetadataOptions::default();
+
+                let Ok(probed) = symphonia::default::get_probe().format(
+                    &hint,
+                    mss,
+                    &format_opts,
+                    &metadata_opts,
+                ) else {
+                    continue;
+                };
+
+                let mut format = probed.format;
+
+                let mut title: Option<String> = Some(file_name.clone());
+                let mut has_audio: bool = false;
+                let mut duration = 0;
+
+                if let Some(metadata) = format.metadata().current() {
+                    for tag in metadata.tags() {
+                        if let Some(StandardTagKey::TrackTitle) = tag.std_key {
+                            title = Some(tag.value.to_string());
+                            break;
+                        }
+                    }
+                };
+
+                for track in format.tracks() {
+                    let params = &track.codec_params;
+
+                    if params.sample_rate.is_some() {
+                        has_audio = true;
+                    }
+
+                    if let (Some(n_frames), Some(tb)) = (params.n_frames, params.time_base) {
+                        let total_seconds = n_frames as f64 * (tb.numer as f64 / tb.denom as f64);
+
+                        if total_seconds as i32 > duration {
+                            duration = total_seconds as i32;
+                        }
+                    }
+                }
+
+                let dest_file_path = get_app_videos_dir().join(PathBuf::from(&file_name));
+
+                let mut video_entity = VideoEntity::from_metadata(VideoMetadata {
+                    file_name: file_name.clone(),
+                    mime_type: file_type.mime_type().to_string(),
+                    file_size,
+                    checksum,
+                    has_audio,
+                    duration,
+                });
+
+                video_entity.title = title;
+
+                match diesel::insert_into(videos::table)
+                    .values(&video_entity)
+                    .execute(&mut conn)
+                {
+                    Ok(_) => {
+                        if std::fs::copy(entry_path, &dest_file_path).is_err() {
+                            continue;
+                        }
+                    }
+
+                    Err(_) => {
+                        continue;
+                    }
                 }
             } else if file_type.matcher_type() == infer::MatcherType::Audio {
                 let Ok(audio_exists) =
