6cfda6bbea27c29ad8cfb2c8c1ac61a5ceb6bd51
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-12 20:21:43 +0300

    use a memo for the asset count

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -15,25 +15,29 @@ type Props = {
 export const Nav: VoidComponent<Props> = (props) => {
     const globalData = useGlobalData();
 
+    const imagesCount = createMemo(
+        () => globalData.resources.images.get()?.length || 0,
+    );
+
+    const videosCount = createMemo(
+        () => globalData.resources.videos.get()?.length || 0,
+    );
+
+    const audioCount = createMemo(
+        () => globalData.resources.audio.get()?.length || 0,
+    );
+
+    const snippetsCount = createMemo(
+        () => globalData.resources.snippets.get()?.length || 0,
+    );
+
     return (
         <div class='flex justify-between'>
             <div class='flex gap-3' ref={props.ref}>
-                <Tab
-                    count={globalData.resources.images.get()?.length || 0}
-                    type='Images'
-                />
-                <Tab
-                    count={globalData.resources.videos.get()?.length || 0}
-                    type='Videos'
-                />
-                <Tab
-                    count={globalData.resources.audio.get()?.length || 0}
-                    type='Audio'
-                />
-                <Tab
-                    count={globalData.resources.snippets.get()?.length || 0}
-                    type='Snippets'
-                />
+                <Tab count={imagesCount()} type='Images' />
+                <Tab count={videosCount()} type='Videos' />
+                <Tab count={audioCount()} type='Audio' />
+                <Tab count={snippetsCount()} type='Snippets' />
             </div>
             <div class='flex gap-5'>
                 <Show when={globalData.store.activeTab === 'Snippets'}>
