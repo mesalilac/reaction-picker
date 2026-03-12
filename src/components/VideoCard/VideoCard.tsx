b2431d446c49a0b06f074ee604671c82261f7bd0
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-12 20:48:59 +0300

    display video

--- src/components/VideoCard/VideoCard.tsx
+++ src/components/VideoCard/VideoCard.tsx
@@ -1,4 +1,5 @@
-import type { VoidComponent } from 'solid-js';
+import { convertFileSrc } from '@tauri-apps/api/core';
+import { createEffect, type VoidComponent } from 'solid-js';
 import type { Video } from '@/bindings';
 
 type Props = {
@@ -7,5 +8,29 @@ type Props = {
 };
 
 export const VideoCard: VoidComponent<Props> = (props) => {
-    return <div ref={props.ref}>{props.video.title}</div>;
+    let videoRef!: HTMLVideoElement;
+
+    createEffect(() => {
+        if (videoRef) videoRef.volume = 0.1;
+    });
+
+    return (
+        <div
+            class='flex flex-col gap-4 rounded-lg bg-neutral-900 p-4'
+            ref={props.ref}
+        >
+            <div class='h-80 w-full self-center'>
+                <video class='h-full w-full rounded-lg' controls ref={videoRef}>
+                    <source
+                        src={convertFileSrc(props.video.filePath)}
+                        type={props.video.mimeType}
+                    />
+                    <track kind='captions' />
+                </video>
+            </div>
+            <div class='flex flex-col'>
+                <span>{props.video.title}</span>
+            </div>
+        </div>
+    );
 };
