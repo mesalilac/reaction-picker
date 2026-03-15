7a3f500e9c45ecea545807df136e894ca40a0913
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-16 01:38:28 +0300

    display more info for images

--- src/components/Main/ImageCard/ImageCard.tsx
+++ src/components/Main/ImageCard/ImageCard.tsx
@@ -2,6 +2,7 @@ import { createVisibilityObserver } from '@solid-primitives/intersection-observe
 import { convertFileSrc } from '@tauri-apps/api/core';
 import { openUrl } from '@tauri-apps/plugin-opener';
 import { decode } from 'blurhash';
+import { filesize } from 'filesize';
 import { createSignal, onMount, Show, type VoidComponent } from 'solid-js';
 import { toast } from 'solid-sonner';
 
@@ -137,7 +138,74 @@ export const ImageCard: VoidComponent<Props> = (props) => {
             </div>
             <div class='flex flex-col gap-4'>
                 <div class='flex flex-col gap-2'>
-                    <CardField label='title'>{props.image.title}</CardField>
+                    <CardField label='title'>
+                        <span title={props.image.title ?? undefined}>
+                            {props.image.title}
+                        </span>
+                    </CardField>
+                    <CardField label='description'>
+                        <span title={props.image.description ?? undefined}>
+                            {props.image.description}
+                        </span>
+                    </CardField>
+                    <CardField label='file path'>
+                        <span title={props.image.fileName ?? undefined}>
+                            {props.image.fileName}
+                        </span>
+                    </CardField>
+                    <CardField label='dimensions'>
+                        <span
+                            title={`${props.image.width}x${props.image.height}`}
+                        >
+                            {props.image.width}x{props.image.height}
+                        </span>
+                    </CardField>
+                    <CardField label='total uses'>
+                        <span
+                            title={
+                                props.image.lastUsedAt
+                                    ? new Date(
+                                          props.image.lastUsedAt,
+                                      ).toLocaleString()
+                                    : undefined
+                            }
+                        >
+                            {props.image.useCounter}
+                        </span>
+                    </CardField>
+                    <CardField label='file size'>
+                        <span title={props.image.fileSize.toString()}>
+                            {filesize(props.image.fileSize)}
+                        </span>
+                    </CardField>
+                    <CardField label='tags'>
+                        {props.image.tags.length > 0 ? (
+                            <span
+                                title={props.image.tags
+                                    .map((tag) => tag.name)
+                                    .join(', ')}
+                            >
+                                {props.image.tags
+                                    .map((tag) => tag.name)
+                                    .join(', ')}
+                            </span>
+                        ) : undefined}
+                    </CardField>
+                    <CardField
+                        label='deleted at'
+                        show={props.image.deletedAt !== null}
+                    >
+                        {props.image.deletedAt ? (
+                            <span class='text-red-500'>
+                                {new Date(
+                                    props.image.deletedAt,
+                                ).toLocaleString()}
+                            </span>
+                        ) : undefined}
+                    </CardField>
+                    <CardField label='added at'>
+                        {new Date(props.image.createdAt).toLocaleString()}
+                    </CardField>
                 </div>
                 <div class='flex flex-row justify-between'>
                     <div class='flex flex-row gap-2'>
