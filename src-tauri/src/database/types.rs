c092ba191f17b49403594c66960a2a2a39115131
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-16 23:03:46 +0300

    use DieselNewType macro

--- src-tauri/src/database/types.rs
+++ src-tauri/src/database/types.rs
@@ -1,11 +1,8 @@
-use diesel::backend::Backend;
-use diesel::deserialize::{self, FromSql, FromSqlRow};
-use diesel::expression::AsExpression;
-use diesel::serialize::{self, Output, ToSql};
-use diesel::sql_types::BigInt;
+use diesel_derive_newtype::DieselNewType;
 use serde::{Deserialize, Serialize};
 
 #[derive(
+    DieselNewType,
     specta::Type,
     Serialize,
     Deserialize,
@@ -16,11 +13,8 @@ use serde::{Deserialize, Serialize};
     Eq,
     PartialOrd,
     Ord,
-    AsExpression,
-    FromSqlRow,
 )]
 #[serde(transparent)]
-#[diesel(sql_type = BigInt)]
 pub struct Timestamp(pub i64);
 
 impl Timestamp {
@@ -32,26 +26,5 @@ impl Timestamp {
         let ms = timestamp.as_millis() as i64;
 
         Self(ms)
-    }
-}
-
-impl<DB> ToSql<BigInt, DB> for Timestamp
-where
-    DB: Backend,
-    i64: ToSql<BigInt, DB>,
-{
-    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, DB>) -> serialize::Result {
-        ToSql::<BigInt, DB>::to_sql(&self.0, out)
-    }
-}
-
-impl<DB> FromSql<BigInt, DB> for Timestamp
-where
-    DB: Backend,
-    i64: FromSql<BigInt, DB>,
-{
-    fn from_sql(bytes: DB::RawValue<'_>) -> deserialize::Result<Self> {
-        let val = i64::from_sql(bytes)?;
-        Ok(Timestamp(val))
     }
 }
