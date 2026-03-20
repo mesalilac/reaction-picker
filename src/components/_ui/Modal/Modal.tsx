daf42fa1d868e60e4d6d944f0979d46d0dac5cc7
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-20 06:25:47 +0300

    add `Modal.body`

--- src/components/_ui/Modal/Modal.tsx
+++ src/components/_ui/Modal/Modal.tsx
@@ -136,6 +136,19 @@ Modal.title = (props: { title: JSX.Element; class?: string }) => {
     );
 };
 
+Modal.body = (props: { class?: string; children: JSX.Element }) => {
+    return (
+        <main
+            class={twMerge(
+                'flex flex-col gap-4 overflow-y-auto p-2',
+                props.class,
+            )}
+        >
+            {props.children}
+        </main>
+    );
+};
+
 Modal.footer = (props: {
     dismiss?: JSX.Element;
     action?: JSX.Element;
