de41e93db037736218b03d2540ce1af421076ab7
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-21 05:58:24 +0300

    set to null if input is empty

--- src/components/Main/EditAssetModal.tsx
+++ src/components/Main/EditAssetModal.tsx
@@ -1,7 +1,7 @@
 import { createStore } from 'solid-js/store';
 
 import type { Audio, Image, Snippet, Video } from '@/bindings';
-import { Modal, type ModalWrapperProps } from '@/components';
+import { Input, Modal, type ModalWrapperProps } from '@/components';
 
 type EditAssetStoreType = {
     title: string | null;
@@ -45,17 +45,25 @@ export const EditAssetModal = (
     });
 
     const onAction = () => {
-        if (store.title === props.item.data.title) setStore('title', null);
-        if (store.description === props.item.data.description)
+        if (store.title === props.item.data.title || store.title?.trim() === '')
+            setStore('title', null);
+        if (
+            store.description === props.item.data.description ||
+            store.description?.trim() === ''
+        )
             setStore('description', null);
         if (
-            props.item.type === 'snippet' &&
-            store.content === props.item.data.content
+            (props.item.type === 'snippet' &&
+                store.content === props.item.data.content) ||
+            store.content?.trim() === ''
         )
             setStore('content', null);
         if (store.useCounter === props.item.data.useCounter)
             setStore('useCounter', null);
-        if (store.externalLink === props.item.data.externalLink)
+        if (
+            store.externalLink === props.item.data.externalLink ||
+            store.externalLink?.trim() === ''
+        )
             setStore('externalLink', null);
 
         props.onSave(store);
@@ -64,7 +72,14 @@ export const EditAssetModal = (
     return (
         <Modal onOpenChange={props.onOpenChange} open={props.open}>
             <Modal.Title title={props.title} />
-            <Modal.Body>hi</Modal.Body>
+            <Modal.Body>
+                <Input
+                    label='title'
+                    onInput={(value) => setStore('title', value?.trim())}
+                    parse={(raw) => String(raw)}
+                    value={store.title ?? ''}
+                />
+            </Modal.Body>
             <Modal.Footer onAction={onAction} />
         </Modal>
     );
