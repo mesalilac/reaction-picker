6f523a096a589fc85176fa613682974572901568
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-29 20:52:49 +0300

    ui: marge props.class

--- src/ui/Badge/Badge.tsx
+++ src/ui/Badge/Badge.tsx
@@ -1,14 +1,19 @@
 import type { JSX, ParentComponent } from 'solid-js';
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
     ref?: HTMLSpanElement | ((el: HTMLSpanElement) => void);
+    class?: string;
     children: JSX.Element;
 };
 
 export const Badge: ParentComponent<Props> = (props) => {
     return (
         <span
-            class='inline-flex items-center justify-center rounded-full bg-neutral-100/10 px-1 font-medium text-neutral-100 text-xs'
+            class={twMerge(
+                'inline-flex items-center justify-center rounded-full bg-neutral-100/10 px-1 font-medium text-neutral-100 text-xs',
+                props.class,
+            )}
             ref={props.ref}
         >
             {props.children}
