b75e61d46f3b313e846487daebfcb3caaa3dc75b
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-01 11:44:09 +0300

    set `<Select.Menu>` min-width to trigger width

--- src/ui/Select/Menu.tsx
+++ src/ui/Select/Menu.tsx
@@ -13,6 +13,14 @@ export type Props = {
 export const Menu = (props: Props) => {
     const ctx = useSelectContext();
 
+    const triggerWidth = (): string => {
+        const ref = ctx.triggerRef();
+
+        if (ref) return `${ref.getBoundingClientRect().width}px`;
+
+        return 'auto';
+    };
+
     return (
         <Popover
             onOpenChange={ctx.setIsOpen}
@@ -23,9 +31,12 @@ export const Menu = (props: Props) => {
         >
             <div
                 class={cn(
-                    'mt-2 flex max-h-80 min-w-80 flex-col gap-1 overflow-y-auto overscroll-contain rounded-lg bg-neutral-800 p-2 text-inherit',
+                    'mt-2 flex max-h-80 flex-col gap-1 overflow-y-auto overscroll-contain rounded-lg bg-neutral-800 p-2 text-inherit',
                     props.class,
                 )}
+                style={{
+                    'min-width': triggerWidth(),
+                }}
             >
                 {props.children}
             </div>
