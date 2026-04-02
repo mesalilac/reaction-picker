bff1a8ac4dcad3bc2711f257d8573bce7db3ef07
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-02 19:28:07 +0300

    mark `<Separator>` as a `voidComponent`

--- src/ui/DropdownMenu/Separator.tsx
+++ src/ui/DropdownMenu/Separator.tsx
@@ -1,5 +1,7 @@
+import type { VoidComponent } from 'solid-js';
+
 import { Separator } from '@/ui';
 
-export const MenuSeparator = () => {
+export const MenuSeparator: VoidComponent = () => {
     return <Separator class='my-2 border-neutral-700' />;
 };
