1b7926b8b0f8cf255c19e4690d6f32e3210914fd
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-03 01:48:52 +0300

    add padding to label

--- src/ui/DropdownMenu/Label.tsx
+++ src/ui/DropdownMenu/Label.tsx
@@ -9,8 +9,10 @@ type Props = {
 
 export const Label = (props: Props) => {
     return (
-        <span class={cn('px-4 py-2 font-bold text-sm', props.class)}>
-            {props.children}
-        </span>
+        <div class='px-4 py-2'>
+            <span class={cn('font-bold text-sm', props.class)}>
+                {props.children}
+            </span>
+        </div>
     );
 };
