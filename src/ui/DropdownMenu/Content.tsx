d4ca1b31ea4019ec8419a818ed9cd05783cc0bb8
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-02 17:45:16 +0300

    add content popover

--- src/ui/DropdownMenu/Content.tsx
+++ src/ui/DropdownMenu/Content.tsx
@@ -1,9 +1,26 @@
 import type { JSX } from 'solid-js';
 
+import { Popover } from '@/ui';
+
+import { useDropdownMenuContext } from './context';
+
 type Props = {
     children: JSX.Element;
 };
 
 export const Content = (props: Props) => {
-    return <div>{props.children}</div>;
+    const ctx = useDropdownMenuContext();
+
+    return (
+        <Popover
+            onOpenChange={ctx.setIsOpen}
+            open={ctx.isOpen()}
+            targetPositionArea='block-end span-inline-end'
+            triggerElement={ctx.triggerRef()}
+        >
+            <div class='mt-1 min-w-30 rounded-lg bg-neutral-800 p-2 text-neutral-200'>
+                {props.children}
+            </div>
+        </Popover>
+    );
 };
