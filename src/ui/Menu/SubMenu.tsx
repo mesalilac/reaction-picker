2b9c0fcd91ac1de05cfcf0ffa0edfa88034f18df
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-02 13:53:12 +0300

    add submenu trigger button

--- src/ui/Menu/SubMenu.tsx
+++ src/ui/Menu/SubMenu.tsx
@@ -1,3 +1,29 @@
-export const SubMenu = () => {
-    return null;
+import type { JSX } from 'solid-js';
+
+import { IconArrowRightMd } from '@/icons';
+import { Button } from '@/ui';
+import { cn } from '@/utils';
+
+type Props = {
+    class?: string;
+    label: JSX.Element;
+    children: JSX.Element;
+};
+
+export const SubMenu = (props: Props) => {
+    const handleClick = () => {};
+
+    return (
+        <Button
+            class={cn(
+                'w-full justify-between text-nowrap capitalize',
+                props.class,
+            )}
+            onClick={handleClick}
+            variant='ghost'
+        >
+            <div class='flex flex-row items-center gap-2'>{props.label}</div>
+            <IconArrowRightMd />
+        </Button>
+    );
 };
