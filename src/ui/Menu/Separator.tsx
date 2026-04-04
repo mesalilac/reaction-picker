9c0781ff447e7aaa7a78a98fb3a211ac96ae4ebb
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-04 18:34:33 +0300

    remove old `Menu` component

--- src/ui/Menu/Separator.tsx
+++
@@ -1,5 +0,0 @@
-import { Separator } from '@/ui';
-
-export const MenuSeparator = () => {
-    return <Separator class='my-2 border-neutral-700' />;
-};
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -6,7 +6,6 @@ export * from './Popover';
 export * from './Select';
 export * from './Input';
 export * from './Textarea';
-export * from './Menu';
 export * from './Modal';
 export * from './Separator';
 export * from './Checkbox';
