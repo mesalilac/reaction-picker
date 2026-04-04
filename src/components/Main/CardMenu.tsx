fd677a051fc70ac13af7295eda2d481fa5f8a532
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-04 18:33:03 +0300

    use new custom menu

--- src/components/Main/CardMenu.tsx
+++ src/components/Main/CardMenu.tsx
@@ -7,7 +7,7 @@ import {
     IconInterfaceTrashFull,
     IconMenuMoreHorizontal,
 } from '@/icons';
-import { Button, Menu, Popover } from '@/ui';
+import { DropdownMenu } from '@/ui';
 
 type Props = {
     handleEditDetails: () => void;
@@ -21,50 +21,36 @@ type Props = {
 };
 
 export const CardMenu = (props: Props) => {
-    let popoverMenuRef!: HTMLButtonElement;
-
     return (
-        <>
-            <Button ref={popoverMenuRef} variant='icon'>
+        <DropdownMenu onOpenChange={props.onOpenChange} open={props.open}>
+            <DropdownMenu.Trigger variant='icon'>
                 <IconMenuMoreHorizontal class='size-5' />
-            </Button>
-            <Popover
-                onOpenChange={props.onOpenChange}
-                open={props.open}
-                targetPositionArea='bottom center'
-                triggerElement={popoverMenuRef}
-            >
-                <Menu onOpenChange={props.onOpenChange} open={props.open}>
-                    <Menu.Item onClick={props.handleEditDetails}>
-                        <IconEditPencil01 /> edit details
-                    </Menu.Item>
-                    <Show when={props.externalLink}>
-                        <Menu.Separator />
-                        <Menu.Item onClick={props.handleOpenExternalLink}>
-                            <IconInterfaceExternalLink /> open external link
-                        </Menu.Item>
-                    </Show>
-                    <Menu.Separator />
-                    <Menu.SubMenu label='Sub menu'>
-                        <Menu.Item>sub menu item</Menu.Item>
-                    </Menu.SubMenu>
-                    <Menu.Separator />
-                    <Show when={props.deletedAt !== null}>
-                        <Menu.Item onClick={props.handleRestore}>
-                            <IconEditUndo /> restore
-                        </Menu.Item>
-                    </Show>
-                    <Menu.Item
-                        class='text-red-500'
-                        onClick={props.handleDelete}
-                    >
-                        <IconInterfaceTrashFull />
-                        {props.deletedAt !== null
-                            ? 'permanently delete'
-                            : 'delete'}
-                    </Menu.Item>
-                </Menu>
-            </Popover>
-        </>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+                <DropdownMenu.Item onClick={props.handleEditDetails}>
+                    <IconEditPencil01 /> edit details
+                </DropdownMenu.Item>
+                <Show when={props.externalLink}>
+                    <DropdownMenu.Separator />
+                    <DropdownMenu.Item onClick={props.handleOpenExternalLink}>
+                        <IconInterfaceExternalLink /> open external link
+                    </DropdownMenu.Item>
+                </Show>
+                <Show when={props.deletedAt !== null}>
+                    <DropdownMenu.Separator />
+                    <DropdownMenu.Item onClick={props.handleRestore}>
+                        <IconEditUndo /> restore
+                    </DropdownMenu.Item>
+                </Show>
+                <DropdownMenu.Separator />
+                <DropdownMenu.Item
+                    class='text-red-500'
+                    onClick={props.handleDelete}
+                >
+                    <IconInterfaceTrashFull />
+                    {props.deletedAt !== null ? 'permanently delete' : 'delete'}
+                </DropdownMenu.Item>
+            </DropdownMenu.Content>
+        </DropdownMenu>
     );
 };
