5ffd7b9dd92594abd86ae18ae1021f0353cb6f5e
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-06 19:43:06 +0300

    refactor `<Checkbox>`

--- src/ui/Checkbox/Checkbox.tsx
+++ src/ui/Checkbox/Checkbox.tsx
@@ -1,46 +1,56 @@
-import type { JSX, VoidComponent } from 'solid-js';
+import { type JSX, Show, type VoidComponent } from 'solid-js';
 
 import { cn } from '@/utils';
 
+import { Helper } from '../DropdownMenu/Helper';
+
 type Props = {
-    checked: boolean;
+    checked?: boolean;
     onChange: (checked: boolean) => void;
+    helper?: JSX.Element;
     label: JSX.Element;
 };
 
 export const Checkbox: VoidComponent<Props> = (props) => {
     return (
-        <div
-            class='flex select-none items-center gap-2'
-            onClick={() => props.onChange(!props.checked)}
-            role='none'
-        >
+        <div class='flex flex-col gap-1'>
             <div
-                class={cn(
-                    'flex size-4 cursor-pointer items-center rounded-sm border transition-colors duration-200 ease-out',
-                    props.checked
-                        ? 'border-blue-500 bg-blue-500'
-                        : 'border-neutral-600',
-                )}
+                class='flex select-none items-center gap-2'
+                onClick={() => props.onChange(!props.checked)}
+                role='none'
             >
-                <svg
-                    class='size-4 transition-opacity duration-100 ease-in-out'
-                    fill='none'
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    style={{
-                        opacity: props.checked ? '1' : '0',
-                    }}
-                    viewBox='0 0 24 24'
-                    xmlns='http://www.w3.org/2000/svg'
+                <div
+                    class={cn(
+                        'flex size-4 cursor-pointer items-center rounded-sm border transition-colors duration-200 ease-out',
+                        props.checked
+                            ? 'border-blue-500 bg-blue-500'
+                            : 'border-neutral-600',
+                    )}
                 >
-                    <title>Check</title>
-                    <path d='M5 12l5 5l10 -10' />
-                </svg>
+                    <svg
+                        class='size-4 transition-opacity duration-100 ease-in-out'
+                        fill='none'
+                        stroke='currentColor'
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        stroke-width='2'
+                        style={{
+                            opacity: props.checked ? '1' : '0',
+                        }}
+                        viewBox='0 0 24 24'
+                        xmlns='http://www.w3.org/2000/svg'
+                    >
+                        <title>Check</title>
+                        <path d='M5 12l5 5l10 -10' />
+                    </svg>
+                </div>
+                <Show when={props.label}>
+                    <span class='select-none text-sm'>{props.label}</span>
+                </Show>
             </div>
-            {props.label}
+            <Show when={props.helper}>
+                <Helper text={props.helper} />
+            </Show>
         </div>
     );
 };
