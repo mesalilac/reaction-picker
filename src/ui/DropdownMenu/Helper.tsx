b95d438c957503fc20508b5174d6682d45813801
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-06 19:47:34 +0300

    add `class` prop to `Helper`

--- src/ui/DropdownMenu/Helper.tsx
+++ src/ui/DropdownMenu/Helper.tsx
@@ -1,9 +1,16 @@
 import type { JSX } from 'solid-js';
 
+import { cn } from '@/utils';
+
 type Props = {
     text: JSX.Element;
+    class?: string;
 };
 
 export const Helper = (props: Props) => {
-    return <span class='text-neutral-500/80 text-xs'>{props.text}</span>;
+    return (
+        <span class={cn('text-neutral-500/80 text-xs', props.class)}>
+            {props.text}
+        </span>
+    );
 };
