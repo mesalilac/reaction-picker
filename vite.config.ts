0bd177f8c8c6b65f4b136faafb04defb15163b49
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-07 00:30:53 +0300

    fix vite checker command

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(async () => ({
     plugins: [
         solid(),
         tailwindcss(),
-        checker({ typescript: true, biome: { watchPath: './src/' } }),
+        checker({ typescript: true, biome: { command: "check" } }),
     ],
     resolve: {
         tsconfigPaths: true,
