c6422c6bc91de59d93e86b964a396201dcdf32de
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-06 19:34:06 +0300

    add helper text for `<ToggleSwitch>`

--- src/ui/ToggleSwitch/ToggleSwitch.tsx
+++ src/ui/ToggleSwitch/ToggleSwitch.tsx
@@ -2,10 +2,13 @@ import { type JSX, Show } from 'solid-js';
 
 import { cn } from '@/utils';
 
+import { Helper } from '../DropdownMenu/Helper';
+
 type Props = {
     checked?: boolean;
     onChange: (value: boolean) => void;
     label?: JSX.Element;
+    helper?: JSX.Element;
     disabled?: boolean;
     class?: string;
     trackClass?: string;
@@ -20,42 +23,47 @@ export const ToggleSwitch = (props: Props) => {
     };
 
     return (
-        <div
-            aria-checked={props.checked}
-            aria-disabled={props.disabled}
-            class={cn(
-                'flex flex-row items-center gap-2 outline-none focus-visible:ring-2 focus-visible:ring-blue-500',
-                props.disabled && 'opacity-60',
-                props.class,
-            )}
-            onClick={handleToggle}
-            onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                    e.preventDefault();
-                    handleToggle();
-                }
-            }}
-            role='switch'
-            tabIndex={0}
-        >
+        <div class='flex flex-col gap-1'>
             <div
+                aria-checked={props.checked}
+                aria-disabled={props.disabled}
                 class={cn(
-                    'flex h-5 w-9 cursor-pointer items-center rounded-full border border-neutral-600 bg-neutral-600/30 p-0.5 transition-colors',
-                    props.checked && 'border-blue-600 bg-blue-500',
-                    props.disabled && 'cursor-auto',
-                    props.trackClass,
+                    'flex flex-row items-center gap-2 outline-none focus-visible:ring-2 focus-visible:ring-blue-500',
+                    props.disabled && 'opacity-60',
+                    props.class,
                 )}
+                onClick={handleToggle}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        e.preventDefault();
+                        handleToggle();
+                    }
+                }}
+                role='switch'
+                tabIndex={0}
             >
                 <div
                     class={cn(
-                        'h-4 w-4 rounded-full bg-white transition-transform duration-200 ease-out',
-                        props.checked && 'translate-x-4',
-                        props.thumbClass,
+                        'flex h-5 w-9 cursor-pointer items-center rounded-full border border-neutral-600 bg-neutral-600/30 p-0.5 transition-colors',
+                        props.checked && 'border-blue-600 bg-blue-500',
+                        props.disabled && 'cursor-auto',
+                        props.trackClass,
                     )}
-                />
+                >
+                    <div
+                        class={cn(
+                            'h-4 w-4 rounded-full bg-white transition-transform duration-200 ease-out',
+                            props.checked && 'translate-x-4',
+                            props.thumbClass,
+                        )}
+                    />
+                </div>
+                <Show when={props.label}>
+                    <span class='select-none text-sm'>{props.label}</span>
+                </Show>
             </div>
-            <Show when={props.label}>
-                <span class='select-none text-sm'>{props.label}</span>
+            <Show when={props.helper}>
+                <Helper text={props.helper} />
             </Show>
         </div>
     );
